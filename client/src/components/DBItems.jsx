94a9351954389cf6cc40a3bbac641a717b757ecf
rjaman09 <kumawataman6378@gmail.com>
2023-04-22 12:31:54 +0530

    Installing Imp Packages

---
+++ client/src/components/DBItems.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MaterialTable from "material-table";
+// import {  }
+
+const DBItems = () => {
+  return (
+    <div className="flex items-center justify-center gap-4 pt-6 w-full">
+      <MaterialTable
+        title=""
+
+      />
+    </div>
+  );
+};
+
+export default DBItems;
